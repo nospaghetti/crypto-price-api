2a98996520a6d1584d936723e77409cf8722678e
Vladimir Ivanov <vladimir@ivanov.click>
2025-11-03 18:10:25 +0400

    Initial commit

--- cmd/crypto-api/main.go
+++ cmd/crypto-api/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/nospaghetti/crypto-price-api/internal/app"
 )
 
 func main() {
-	a := &app.App{}
+	fmt.Println("Connecting to database...")
+	db, err := pgxpool.New(context.Background(), "postgres://user:pass@localhost:5432/mydb")
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close()
 
+	fmt.Println("Successfully connected to database")
+	fmt.Println("Setting up server...")
+
+	a := &app.App{db}
 	mux := http.NewServeMux()
 	a.SetupRoutes(mux)
 
-	err := http.ListenAndServe(":80", mux)
+	err = http.ListenAndServe(":80", mux)
 
 	if err != nil {
 		_ = fmt.Errorf("failed to start server: %v", err)
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,12 +1,15 @@
 package app
 
 import (
+	"github.com/jackc/pgx/v5/pgxpool"
 	v1health "github.com/nospaghetti/crypto-price-api/internal/handlers/health/v1"
 	v1history "github.com/nospaghetti/crypto-price-api/internal/handlers/history/v1"
 	v1prices "github.com/nospaghetti/crypto-price-api/internal/handlers/prices/v1"
 )
 
 type App struct {
+	DB *pgxpool.Pool
+
 	V1 struct {
 		Health  *v1health.HandlerV1
 		Prices  *v1prices.HandlerV1
